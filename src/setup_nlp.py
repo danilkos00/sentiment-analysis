dfcb41cff261b2819bf9a72895a7c06e57718c03
Danil Kostromin <danil1kostr@gmail.com>
2025-04-18 14:09:06 +0300

    setup_nlp.py was changed

--- src/setup_nlp.py
+++ src/setup_nlp.py
@@ -1,8 +1,5 @@
-import re
 import nltk
 import spacy
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from spacy.cli import download as spacy_download
 
 try:
@@ -11,10 +8,6 @@ except OSError:
     spacy_download('en_core_web_sm')
     nlp = spacy.load('en_core_web_sm')
 
-nltk_packages = ['punkt', 'stopwords']
-
-for package in nltk_packages:
-    try:
-        nltk.data.find(f'tokenizers/{package}' if package == 'punkt' else f'corpora/{package}')
-    except LookupError:
-        nltk.download(package)
+nltk.download('punkt_tab')
+nltk.download("stopwords")
+nltk.download('punkt')
