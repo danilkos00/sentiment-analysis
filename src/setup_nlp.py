33521bb3f097ad1ba7463402688792fcfc3fa37e
Danil Kostromin <danil1kostr@gmail.com>
2025-04-18 14:16:46 +0300

    setup_nlp was changed

--- src/setup_nlp.py
+++ src/setup_nlp.py
@@ -1,6 +1,10 @@
+import re
 import nltk
 import spacy
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from spacy.cli import download as spacy_download
+
 
 try:
     nlp = spacy.load('en_core_web_sm')
